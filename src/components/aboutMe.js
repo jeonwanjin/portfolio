b85036f0227721b00ddabf926edd4de8c3a20208
Jeon Wan-jin <jwj1212121@gmail.com>
2024-01-21 19:12:21 +0900

    반응형작업중 1-21

--- src/components/aboutMe.js
+++ src/components/aboutMe.js
@@ -39,7 +39,7 @@ const AboutMe = ({updateShowClick }) => {
         <div className="subAboutMeTit">
             <p>About Me</p> 
         </div>
-        <div className='flex'>
+        <div className='aboutFlex'>
             <div className='profile'>
               <div><img className='jin' src={jin} alt='jin'></img></div>
               <p>Profile</p>
@@ -51,51 +51,51 @@ const AboutMe = ({updateShowClick }) => {
             <div className='profile'>
               <div className='education'>
                 <p>Education</p>
-                <div className='flex justi'>
+                <div className='aboutFlex justi'>
                   <p>2008.02</p><p>동서대학교 게임공학 전공 입학</p>
                 </div>
-                <div className='flex justi'>
+                <div className='aboutFlex justi'>
                   <p>2015.02</p><p>동서대학교 게임공학 전공 졸업</p>
                 </div>
-                <div className='flex justi'>
+                <div className='aboutFlex justi'>
                   <p></p><p>애니메이션 복수전공 졸업</p>
                 </div>
               </div>
               <div className='experiece maginTop50'>
                 <p>Experiece</p>
-                <div className='flex justi'>
+                <div className='aboutFlex justi'>
                   <p>2020.12~2022.11</p><p>자영업</p>
                 </div>
-                <div className='flex justi'>
+                <div className='aboutFlex justi'>
                   <p>2017.03~2018.08</p><p>에이제이트레이드</p>
                 </div>
-                <div className='flex justi'>
+                <div className='aboutFlex justi'>
                   <p>2016.01~2016.12</p><p>나비스오토모티브시스템즈</p>
                 </div>
               </div>
-              <div className='flex justi'>
+              <div className='aboutFlex justi'>
                 <p>2015.02~2015.06</p><p>파크이에스엠</p>
               </div>
-              <div className='flex justi maginTop50'>
+              <div className='aboutFlex justi maginTop50'>
               <p>2023.05~2023.12</p><p>그린컴퓨터아카데미 노원</p>
               </div>
-              <div className='flex justi'>
+              <div className='aboutFlex justi'>
                 <p>2021.04~2021.10</p><p>그린컴퓨터아카데미 부산</p>
               </div>
-              <div className='flex justi'>
+              <div className='aboutFlex justi'>
                 <p>2018.06~2018.08</p><p>코코커피바리스타학원</p>
               </div>
-              <div className='flex justi'>
+              <div className='aboutFlex justi'>
                 <p>2016.06~2016.09</p><p>신세계제과제빵전문학원</p>
               </div>
-              <div className='flex justi'>
+              <div className='aboutFlex justi'>
                 <p>2015.06~2015.09</p><p>노라노디자인아카데미학원</p>
               </div>
             </div>
             <div className='profile'>
               <div className='skills'>
                   <p>Skills</p>
-                  <div className='flex justi'>
+                  <div className='aboutFlex justi'>
                     <p className='font800'>Front-End:</p>
                   </div>
                   <div className='flex maginTop20'>
@@ -121,19 +121,19 @@ const AboutMe = ({updateShowClick }) => {
                   </div>
                   <div className='education maginTop60'>
                     <p>ETC</p>
-                    <div className='flex justi'>
+                    <div className='aboutFlex justi'>
                       <p>2015.10</p><p>아시아청소년영화제 대상 수상 </p>
                     </div>
-                    <div className='flex justi'>
+                    <div className='aboutFlex justi'>
                       <p>2014.11</p><p>게임 창작 챌린지 장려상 수상</p>
                     </div>
-                    <div className='flex justi maginTop20'>
+                    <div className='aboutFlex justi maginTop20'>
                       <p>2018.08</p><p>바리스타2급</p>
                     </div>
-                    <div className='flex justi'>
+                    <div className='aboutFlex justi'>
                       <p>2016.12</p><p>AutoCAD2급 기술자격시험</p>
                     </div>
-                    <div className='flex justi'>
+                    <div className='aboutFlexs justi'>
                       <p>2012.11</p><p>자동차운전면허</p>
                     </div>
                   </div>
